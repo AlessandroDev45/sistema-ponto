a8522a06151e583ee6792c371d349b40b9615eb5
AlessandroDev45 <aaswilel@gmail.com>
2026-01-26 23:01:34 -0300

    Stabilize Chrome on GitHub Actions

--- src/automacao/ponto_controller.py
+++ src/automacao/ponto_controller.py
@@ -42,11 +42,15 @@ class AutomacaoPonto:
 
             chrome_options = Options()
             if self.headless:
-                chrome_options.add_argument('--headless=new')
+                chrome_options.add_argument('--headless')
             if self.incognito:
                 chrome_options.add_argument('--incognito')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--window-size=1920,1080')
+            chrome_options.add_argument('--disable-features=VizDisplayCompositor')
+            chrome_options.add_argument('--remote-debugging-port=9222')
             chrome_options.add_experimental_option(
                 "prefs",
                 {"profile.default_content_setting_values.geolocation": 2}
